3ac6b23d4e64cd8d27cb40506461535aebca504e
Rafaela Dias <93568221+rafaeladns@users.noreply.github.com>
2022-03-15 21:52:34 -0300

    Criando rota post fornecedores

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 //Importando express//
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 //criando rota para o app//
 app.get('/', function(req, res){ //requisicao e resposta//
   res.send('Olá mundo!')
-})
+});
+
+//criando rota post//
+app.post('/fornecedores', function(req, res){
+  console.log(req.body);
+  res.json({
+    "statusCode":200
+  })
+});
 
 //Porta em que a aplicação estará rodando//
 app.listen(5000, ()=> console.log("Api rodando"))
