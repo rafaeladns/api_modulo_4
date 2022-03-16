3684835e70d6af452613e486a8b112b5fd5ba6c1
Rafaela Dias <93568221+rafaeladns@users.noreply.github.com>
2022-03-15 21:20:23 -0300

    Instalação de pacotes ok e add git ignore na node_modules

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("Oi");
+//Importando express//
+const express = require('express');
+const app = express();
+
+//criando rota para o app//
+app.get('/', function(req, res){ //requisicao e resposta//
+  res.send('Olá mundo!')
+})
+
+//Porta em que a aplicação estará rodando//
+app.listen(5000, ()=> console.log("Api rodando"))
